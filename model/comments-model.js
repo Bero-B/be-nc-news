b52960d867d12d5d5acc1e5877391d9d24d59ff5
Bero-B <3berob@gmail.com>
2024-07-16 15:50:01 +0100

    completed task 9 deleteComment

--- model/comments-model.js
+++ model/comments-model.js
@@ -1,10 +1,22 @@
 const db = require('../db/connection')
 
-function insertComment(article_id, body, username) {
+function insertComment(article_id, reqBody) {
+    const {body, username} = reqBody
+    const reqKeys = Object.keys(reqBody)
+    if (reqKeys.length > 2){
+        return Promise.reject({status: 400, msg: "Bad request"})
+    }
     return db.query(`INSERT INTO comments (article_id, body, author) VALUES ($1, $2, $3) RETURNING*;`, [article_id, body, username])
     .then(({rows}) => {
         return rows[0].body
     })
 }
-
-module.exports = insertComment
+function deleteComment(comment_id) {
+    return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id])
+    .then((result) => {
+        if(result.rowCount === 0) {
+            return Promise.reject({status: 404, msg: "Not Found"})
+        }
+    })
+}
+module.exports ={insertComment, deleteComment}
